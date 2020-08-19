d2783e8c8576e3bb242f074034e97372db98a2ea
Sideway <dev.sideway@gmail.com>
2020-08-19 11:07:55 +0200

    data: Add ghidra version of pspsdk header

--- data/pspsdk_ghidra.h
+++ data/pspsdk_ghidra.h
@@ -1,3 +1,13 @@
+typedef signed char int8_t;
+typedef unsigned char uint8_t;
+typedef signed short int int16_t;
+typedef unsigned short int uint16_t;
+typedef signed int int32_t;
+typedef unsigned int uint32_t;
+typedef signed long int int64_t;
+typedef unsigned long int uint64_t;
+
+
 typedef volatile uint8_t vu8;
 typedef volatile uint16_t vu16;
 typedef volatile uint32_t vu32;
